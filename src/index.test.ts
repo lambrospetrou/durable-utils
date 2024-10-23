e928c7809ea47bef7fa5bae4fd9a0630d2cb54d7
Lambros Petrou <lambrospetrou@gmail.com>
2024-10-24 00:18:16 +0100

    initial setup

---
+++ src/index.test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from 'vitest';
+import { add } from './index.ts';
+
+test('adds two numbers', () => {
+  expect(add(1, 2)).toBe(3);
+});
---
+++ tools/build.mjs
@@ -0,0 +1,31 @@
+import { readFileSync } from "fs";
+import { build } from "esbuild";
+
+const {
+  dependencies, peerDependencies
+} = readFileSync("./package.json", "utf-8");
+
+const sharedConfig = {
+  entryPoints: ["src/index.ts", "src/sql-migrations.ts"],
+  bundle: true,
+  minify: true,
+  external: Object.keys(dependencies ?? {}).concat(Object.keys(peerDependencies ?? {})),
+};
+
+build({
+  ...sharedConfig,
+  platform: 'node', // for CJS
+  outdir: "dist/",
+});
+
+build({
+  ...sharedConfig,
+  outdir: "dist/",
+  outExtension: {
+    ".js": ".esm.js"
+  },
+  platform: 'neutral', // for ESM
+  format: "esm",
+});
+
+// Adapted from https://janessagarrow.com/blog/typescript-and-esbuild/
