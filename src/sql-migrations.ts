e0b91ca0bc9884023fecd2ac78fa847e5be7db17
Lambros Petrou <lambrospetrou@gmail.com>
2024-10-24 10:41:01 +0100

    add comments

--- src/sql-migrations.ts
+++ src/sql-migrations.ts
@@ -1,21 +1,79 @@
 import type { DurableObjectStorage } from "@cloudflare/workers-types";
 
+// Represents a single SQL schema migration to run.
 export interface SQLSchemaMigration {
+    // Each migration is identified by this number. You should always use monotonically
+    // increasing numbers for any new migration added, and never change already applied
+    // migrations.
+    // If you modify the `sql` statement of an already applied migration it will not be applied.
     idMonotonicInc: number;
+
+    // Just a description for you, the coder, to know what the migration is about.
+    // This is not used in any way by the migrations runner.
     description: string;
 
+    // The SQL statement to execute for a single schema migration.
+    // Can be multiple statements separated by semicolon (`;`).
+    // This statement is passed directly to the `storage.sql.exec()` function.
+    // See https://developers.cloudflare.com/durable-objects/api/storage-api/#sqlexec
+    //
+    // You should always try and make your SQL statements to be safe to run multiple times.
+    // Even though `SQLSchemaMigrations.runAll()` keeps track of the last migration ID and
+    // never runs anything less than that ID, it's best practice for your statements to be defensive.
+    // For example use `CREATE TABLE IF NOT EXISTS` instead of `CREATE TABLE`.
+    //
+    // Also, you should never change the `sql` code of an already ran migration. Add a new entry
+    // in the `SQLSchemaMigrationsConfig.migrations` list altering the schema as you wish.
+    //
+    // In majority of cases you should always provide this. In the cases where you need to dynamically
+    // fetch your statements at runtime, the `SQLSchemaMigrations.runAll` function accepts
+    // a function (`genSql`) that will be called when migrations are executed and based on the `idMonotonicInc`
+    // you can return the SQL statement to run at that point.
+    // If `sql` is provided, then that callback function is not called even if provided.
     sql?: string;
 }
 
 export interface SQLSchemaMigrationsConfig {
+    // The `DurableObjectState.storage` property of your Durable Object instance.
+    // Usually the `ctx` argument in your DO class constructor, or `state` in some codebases.
+    // See https://developers.cloudflare.com/durable-objects/api/state/#storage
     doStorage: DurableObjectStorage;
+
+    // The list of SQL schema migrations to run in the SQLite database of the Durable Object instance.
+    // Once the given migrations run at least once, you should never modify existing entries,
+    // otherwise you risk corrupting your database, since already ran migrations, will NOT run again.
     migrations: SQLSchemaMigration[];
 
-    __lastAppliedMigrationMonotonicID_OVERRIDE_FOR_MANUAL_MIGRATIONS?: number;
+    // This is the key that will be used to track the last successful migration ID in the KV storage.
+    // You should never have to use this, unless you were using an older version of `SQLSchemaMigrations`
+    // that was using a different key by default, and therefore you would like your migrations to
+    // continue using that, otherwise your migrations would run again being tracked with the new key.
+    //
+    // If your SQL migrations are safe to be ran multiple times, then just don't provide this.
+    // For example, using `CREATE TABLE IF NOT EXISTS` is safe, whereas `CREATE TABLE` along is not.
+    keyNameTrackingLastMigrationID?: string;
 }
 
+/**
+ * SQLSchemaMigrations is a simple class to manage your SQL migrations when using SQLite Durable Objects (DO).
+ *
+ * It accepts a config with the Durable Object `storage`, and a list of migrations.
+ * This list of migrations should cover everything migration ran ever, not just new ones to run.
+ *
+ * Each migration is identified by a monotonically increasing identifier (`idMonototnicInc`).
+ * Once the `runAll()` function has been called at least once, the migrations processed should never
+ * change from that point on. Otherwise, if you change the SQL statement of an already ran migration
+ * that change will not be applied.
+ *
+ * All SQL schema changes should be done with newly added migration entries in the given config, along
+ * with a higher `idMonototnicInc`.
+ *
+ * Running `runAll()` multiple times is safe, and returns early if the migrations array has no changes,
+ * therefore it's recommended to always run it before your Durable Object instance is going to read or
+ * write into the SQLite database, or at least run it once in the constructor of your DO.
+ */
 export class SQLSchemaMigrations {
-    _config: Omit<SQLSchemaMigrationsConfig, "__lastAppliedMigrationMonotonicID_OVERRIDE_FOR_MANUAL_MIGRATIONS">;
+    _config: SQLSchemaMigrationsConfig;
     _migrations: SQLSchemaMigration[];
 
     _lastMigrationMonotonicId: number = -1;
@@ -38,17 +96,11 @@ export class SQLSchemaMigrations {
 
         this._migrations = migrations;
 
-        if (config.__lastAppliedMigrationMonotonicID_OVERRIDE_FOR_MANUAL_MIGRATIONS) {
-            this._config.doStorage.put<number>(
-                "__sql_migrations_lastID",
-                config.__lastAppliedMigrationMonotonicID_OVERRIDE_FOR_MANUAL_MIGRATIONS,
-            );
-        }
-
         // TODO Should we load the `_lastMigrationMonotonicId` from storage here?
         //      Without loading it, `hasMigrationsToRun()` will always return true until `runAll()` runs once.
         //      That's not bad per se, since everyone should `runAll()` as soon as they need their storage
         //      to be writeable.
+        //      Also, if we want to do this, we cannot do it here, since we need to be within `async` function. :(
     }
 
     hasMigrationsToRun() {
@@ -58,7 +110,16 @@ export class SQLSchemaMigrations {
         return this._lastMigrationMonotonicId !== this._migrations[this._migrations.length - 1].idMonotonicInc;
     }
 
-    async runAll(sqlGen?: (idMonotonicInc: number) => string) {
+    /**
+     * Runs all the migrations that haven't already ran. The `idMonotonicInc` of each migration is used
+     * to track which migrations ran or not. New migrations should always have higher `idMonotonicInc`
+     * than older ones!
+     *
+     * @param sqlGen An optional callback function to generate the SQL statement of a given migration at runtime.
+     *               If the migration entry already has a valid `sql` statement this callback is NOT called.
+     * @returns The numbers of rows read and written throughout the migration execution.
+     */
+    async runAll(sqlGen?: (idMonotonicInc: number) => string): Promise<{ rowsRead: number; rowsWritten: number }> {
         const result = {
             rowsRead: 0,
             rowsWritten: 0,
@@ -68,7 +129,8 @@ export class SQLSchemaMigrations {
             return result;
         }
 
-        this._lastMigrationMonotonicId = (await this._config.doStorage.get<number>("__sql_migrations_lastID")) ?? -1;
+        this._lastMigrationMonotonicId =
+            (await this._config.doStorage.get<number>(this._lastMigrationIDKeyName())) ?? -1;
 
         // Skip all the applied ones.
         let idx = 0,
@@ -105,9 +167,13 @@ export class SQLSchemaMigrations {
 
             this._lastMigrationMonotonicId = _lastMigrationMonotonicId;
 
-            await this._config.doStorage.put<number>("__sql_migrations_lastID", this._lastMigrationMonotonicId);
+            await this._config.doStorage.put<number>(this._lastMigrationIDKeyName(), this._lastMigrationMonotonicId);
         });
 
         return result;
+    }
+
+    _lastMigrationIDKeyName() {
+        return this._config.keyNameTrackingLastMigrationID ?? "__sql_migrations_lastID";
     }
 }
